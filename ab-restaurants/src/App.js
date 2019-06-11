b59cec72947be738b91cd7e5c72660ace6e606a6
sathiyaraj <sathiyaraj45@gmail.com>
2019-06-11 19:09:30 +0530

    react project folder added

---
+++ ab-restaurants/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">      
+    </div>
+  );
+}
+export default App;
